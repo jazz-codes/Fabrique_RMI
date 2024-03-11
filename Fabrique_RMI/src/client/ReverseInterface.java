fa1fea47afcc31958adac155955cb7e207d0ef18
jazz-codes <152726047+jazz-codes@users.noreply.github.com>
2024-03-11 05:53:40 +0100

    Add files via upload

---
+++ Fabrique_RMI/src/client/ReverseInterface.java
@@ -0,0 +1,9 @@
+package client;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+public interface ReverseInterface extends Remote{
+
+public  String reverseString(String str) throws RemoteException;
+
+}
